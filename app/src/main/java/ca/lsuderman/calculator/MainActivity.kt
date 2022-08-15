e166d6e6a44938a5badfa9402412f8acc72ad38b
Leighton <ljsuderman7@gmail.com>
2022-08-15 16:44:52 -0400

    testing

--- app/src/main/java/ca/lsuderman/calculator/MainActivity.kt
+++ app/src/main/java/ca/lsuderman/calculator/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn0 = findViewById<Button>(R.id.btn0);
+        val btn1 = findViewById<Button>(R.id.btn1);
     }
 }
